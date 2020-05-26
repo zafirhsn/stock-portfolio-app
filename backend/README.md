f5c21432f4e5d325667b72044b8b4a6788c34eb2
Zafir Hasan <zafirhsn@gmail.com>
2020-05-26 16:49:57 -0400

    :rocket: Deploy

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+### Install dependencies
+```
+npm install
+```
+
+### Run server
+```
+npm run dev
+```
+
+### API Documentation in [/docs](https://github.com/zafirhsn/stock-portfolio-app/tree/master/backend/docs)
--- backend/services/buy.js
+++ backend/services/buy.js
@@ -98,7 +98,7 @@ module.exports = async (ticker, quantity, payload) => {
 
       let previous = await requestPromise.get(url);
       previous = JSON.parse(previous);
-      ohlc = {
+      ohlc[ticker] = {
         open: previous.open,
         close: previous.close
       }
